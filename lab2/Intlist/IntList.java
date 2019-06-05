1bbbe19c71ae677093bf57f361e869c99a8631c4
dengyi <dengyi@usc.edu>
2019-06-05 21:01:52 +0800

    completed version2 of lab2

--- lab2/Intlist/IntList.java
+++ lab2/Intlist/IntList.java
@@ -82,6 +82,9 @@ public class IntList {
 
     public static IntList dcatenate(IntList A, IntList B) {
         //TODO:  fill in method
+        if (A == null) {
+            return B;
+        }
         IntList temp = A;
         while (temp.rest != null) {
             temp = temp.rest;
@@ -96,6 +99,9 @@ public class IntList {
      */
     public static IntList catenate(IntList A, IntList B) {
         //TODO:  fill in method
+        if (A == null) {
+            return B;
+        }
         IntList res = new IntList(A.first, null);
         IntList ptr = res;
         A = A.rest;
