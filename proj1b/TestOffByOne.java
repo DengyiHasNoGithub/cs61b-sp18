725e6eb4fedb8d1549999fc944d69f5ad6d10970
dengyi <dengyi@usc.edu>
2019-06-21 11:25:40 +0800

    completed 2nd version

--- proj1b/TestOffByOne.java
+++ proj1b/TestOffByOne.java
@@ -8,11 +8,15 @@ public class TestOffByOne {
 
     @Test
     public void testEqualChars() {
-        boolean s1 = offByOne.equalChars('x', 'w');
+        boolean s1 = offByOne.equalChars('w', 'x');
         assertTrue(s1);
         boolean s2 = offByOne.equalChars('&', '%');
         assertTrue(s2);
         boolean s3 = offByOne.equalChars('a', 'c');
         assertFalse(s3);
+        boolean s4 = offByOne.equalChars('x', 'w');
+        assertTrue(s4);
+        boolean s5 = offByOne.equalChars('A', 'B');
+        assertTrue(s5);
     }
 }
